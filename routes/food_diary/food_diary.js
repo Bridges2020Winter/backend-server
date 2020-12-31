cbf3a18998b6e5e7fbf4c3e107d12c423d8ff182
jack <sungjai2@illinois.edu>
2020-12-31 15:20:35 +0900

    Food diary removal

--- routes/food_diary/food_diary.js
+++ routes/food_diary/food_diary.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const foodDiarySchema = require("../../schemas/foodDiarySchema");
 const router = express.Router();
 const diaryModel = require("../../schemas/foodDiarySchema");
 const authentication = require("../middleware/authentication");
@@ -24,6 +25,48 @@ router.post("/addFood", authentication, async (req, res) => {
             }
         );
         res.status(200).send("Sucessfully inserted food into food diary");
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+//
+/* JSON Format
+{
+    "email": "emial@email.email" ,
+    "password": "password",
+    "id": "ididididididididiid"
+}
+*/
+router.post("/removeFood", authentication, async (req, res) => {
+    try {
+        try {
+            var a =await diaryModel.findOne({
+                email: req.body.email
+            });
+            console.log("why?", a)
+            var b = await a.diary.findOne({_id: req.body.id});
+            console.log("yyy?", b);
+        } catch (error) {
+            // food with such id does nto exist
+            console.log("not valid is", error)
+            res.status(600).send("Id is not valid");
+            return 
+        }
+
+        await diaryModel.updateOne(
+            {
+                email: req.body.email
+            },
+            {
+                $pull: {
+                    diary: {
+                        _id: req.body.id
+                    }
+                }
+            }
+        )
+        res.status(200).send("Successfully removed food");
     } catch (error) {
         res.status(500).send(error);
     }
